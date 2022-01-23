4ad0388a076b5694fd7a2845d9704dbd525c72b5
krsharipov <krsharipov@domclick.ru>
2022-01-23 09:02:41 +0300

    Homework 10

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -1,4 +1,5 @@
 public interface Versions {
     String guava = "30.1.1-jre";
     String springBootDependencies = "2.5.2";
+    String testcontainers = "1.15.3";
 }
---
+++ hw10-jpql/src/main/java/ru/sharipov/crm/model/Client.java
@@ -0,0 +1,103 @@
+package ru.sharipov.crm.model;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "client")
+public class Client implements Cloneable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "client_seq_generator")
+    @SequenceGenerator(name = "client_seq_generator", sequenceName = "client_seq", allocationSize = 1)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToOne(mappedBy = "client", cascade = CascadeType.ALL)
+    private Address address;
+
+    @OneToMany(mappedBy = "client", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Phone> phones;
+
+    public Client() {
+    }
+
+    public Client(String name) {
+        this.name = name;
+    }
+
+    public Client(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Client(Long id, String name, Address address, List<Phone> phones) {
+        this.id = id;
+        this.name = name;
+        setAddress(address);
+        phones.forEach(this::addPhone);
+    }
+
+    @Override
+    public Client clone() {
+        return new Client(this.id, this.name);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        if (address == null) {
+            if (this.address != null) {
+                this.address.setClient(null);
+            }
+        } else {
+            address.setClient(this);
+        }
+        this.address = address;
+    }
+
+    public void addPhone(Phone phone) {
+        if (phone == null) {
+            throw new IllegalArgumentException("Phone is null");
+        }
+        if (phones == null) {
+            phones = new ArrayList<>();
+        }
+        phones.add(phone);
+        phone.setClient(this);
+    }
+
+    public List<Phone> getPhones() {
+        return phones;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
